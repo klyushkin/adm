9df2c23dcc270d96af45b20a56aa32ef2a692639
klyushkin <alexandr.klyushkin@gmail.com>
2015-03-12 20:20:01 +0300

    override

--- src/main/java/ru/spbstu/telematics/labs/SortedSet.java
+++ src/main/java/ru/spbstu/telematics/labs/SortedSet.java
@@ -9,6 +9,31 @@ package ru.spbstu.telematics.labs;
  *
  * @author alexandr
  */
-public class SortedSet {
-    
+public class SortedSet implements ISortedSet {
+
+    /**
+     * Добавить элемент в дерево
+     *
+     */
+    @Override
+    public void add(Comparable e) {
+
+    }
+
+    /**
+     * Удалить элемент из дерева
+     */
+    @Override
+    public boolean remove(Comparable o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Возвращает true, если элемент содержится в дереве
+     */
+    @Override
+    public boolean contains(Comparable o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
